f5bdbfaea801cacae6cb47cd08499028e1299426
Aditya Singh <adirocksh@gmail.com>
2024-04-14 17:17:55 +0530

    Update deploy_to_ecs.sh

--- deploy_to_ecs.sh
+++ deploy_to_ecs.sh
@@ -6,11 +6,11 @@ SERVICE_NAME="ccf-platform"
 NEW_DOCKER_IMAGE="903054967221.dkr.ecr.us-east-1.amazonaws.com/ccf-platform:${BUILD_NUMBER}"
 CLUSTER_NAME="ccf-platform"
 
-
 # Export the AWS credentials as environment variables
-ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-AWS_REGION='us-east-1'
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION='us-east-1'
+
 # Fetch the current task definition JSON
 OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY)
 
@@ -36,7 +36,7 @@ if [ -z "$NEW_TASK_DEF" ]; then
 fi
 
 # Extract only required fields for registering the new task definition
-FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
+FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions | map( (. + {"memory": "512", "memoryReservation": "256"}) ) }')
 
 echo "FINAL_TASK:"
 echo "$FINAL_TASK"
