d7f1268c7c40e87a0896ec1903da9a5d6e3e66ce
Aditya Singh <adirocksh@gmail.com>
2024-04-14 16:49:40 +0530

    Update deploy_to_ecs.sh

--- deploy_to_ecs.sh
+++ deploy_to_ecs.sh
@@ -9,11 +9,20 @@ CLUSTER_NAME="ccf-platform"
 # Fetch the current task definition JSON
 OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --region us-east-1)
 
+echo "OLD_TASK_DEF:"
+echo "$OLD_TASK_DEF"
+
 # Update the image in the task definition JSON
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
 
+echo "NEW_TASK_DEF:"
+echo "$NEW_TASK_DEF"
+
 # Extract only required fields for registering the new task definition
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
+
+echo "FINAL_TASK:"
+echo "$FINAL_TASK"
 
 # Register the new task definition
 aws ecs register-task-definition --family $TASK_FAMILY --region us-east-1 --cli-input-json "$FINAL_TASK"
